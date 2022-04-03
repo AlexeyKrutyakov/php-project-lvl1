9533de117bf271f2b5f8218fe1868491f8c4bfc0
arctikbear <coder.arctik@gmail.com>
2022-04-03 12:23:53 +0300

    add file comments

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,26 @@
 <?php
 
+/**
+ * This file content functions for brain-games
+ * 
+ * PHP version 8
+ * 
+ * @category PHP
+ * @package  Brain\Games\Cli
+ * @author   Alexey Coder-Arctik <arctik.coder@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     http://coder-arctik.com
+ */
 namespace Brain\Games\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
+/**
+ * Greetings user
+ * 
+ * @return nothing
+ */
 function greeting()
 {
     line('Welcome to the Brain Game!');
